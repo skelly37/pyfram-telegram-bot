4ab22c94b5c8b7dc30207b39dd26de84faf293a8
mk00pl <83863599+mk00pl@users.noreply.github.com>
2022-02-05 14:30:42 +0100

    Fixed some url related stuff

--- api_calls.py
+++ api_calls.py
@@ -1,26 +1,24 @@
 from requests import get
 from urllib.request import urlretrieve
+from urllib.parse import quote_plus
 
-appID = #your appID
+appID = # your appID
 
 def short_anwser(query):
 	query = "https://api.wolframalpha.com/v1/result?appid=" + appID + "&i=" + query
 	return get(query).text
 
-def get_image(query):
-	filename = query + ".png"
-	
-	query = "https://api.wolframalpha.com/v1/simple?appid=" + appID + "&i=" + query
-	query += "&background=F5F5F5"
-	query += "&fontsize=20"
-	
-	urlretrieve(query, filename)
-	return filename
+def get_image(query,filename):
+        filename = "{}.png".format(filename)
+        api_url = "https://api.wolframalpha.com/v1/simple?appid={}&i={}&background=F5F5F5&fontsize=20"
+        query = api_url.format(appID,quote_plus(query))
+        urlretrieve(query, filename)
+        return filename
 
 def query_wolfram(query, is_image=False):
 	if not is_image:
 		out = short_anwser(query)
 		if out.strip() != "No short answer available":
 			return out
-
+                    
 	return "Query result saved in: " + get_image(query)
