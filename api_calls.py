a2aff9f457fb67dba9e45683a461b7b5a6f0d3d6
Kamil <skelly37@protonmail.com>
2022-02-05 15:06:33 +0000

    more visible hermetization, test cases included

--- api_calls.py
+++ api_calls.py
@@ -2,27 +2,35 @@ from requests import get
 from urllib.request import urlretrieve
 from urllib.parse import quote_plus
 
+
 class Bot:
+    def __init__(self, appID):
+        self.appID = appID
 
-    def __init__(self,appid):
-        self.appid = appid        
+    def __short_anwser(self, sequery):
+        url = "https://api.wolframalpha.com/v1/result?appid={}&i={}"
+        query = url.format(self.appID, quote_plus(sequery))
+        return get(query).text
 
-    def short_answer(self,sequery):
-            url = "https://api.wolframalpha.com/v1/result?appid={}&i={}"
-            query = url.format(self.appid,quote_plus(sequery))
-            return get(query).text
+    def __get_image(self, query):
+        filename = "{}.png".format(query)
+        api_url = "https://api.wolframalpha.com/v1/simple?appid={}&i={}&background=F5F5F5&fontsize=20"
+        query = api_url.format(self.appID, quote_plus(query))
+        urlretrieve(query, filename)
+        return filename
 
-    def get_image(self,query,filename):
-            filename = "{}.png".format(filename)
-            api_url = "https://api.wolframalpha.com/v1/simple?appid={}&i={}&background=F5F5F5&fontsize=20"
-            query = api_url.format(self.appID,quote_plus(query))
-            urlretrieve(query, filename)
-            return filename
+    def query_wolfram(self, query, is_image=False):
+        if not is_image:
+            out = self.__short_anwser(query)
+            if out.strip() != "No short answer available":
+                return out
 
-    def query_wolfram(self,query, is_image=False):
-            if not is_image:
-                    out = short_anwser(query)
-                    if out.strip() != "No short answer available":
-                            return out
-                        
-            return "Query result saved in: " + get_image(query)
+        return "Query result saved in: " + self.__get_image(query)
+
+def temp_test(self):
+    b = Bot("APP_ID")
+    print(f"2+2= {b.query_wolfram('2+2')}")
+    print(f"Poland (should return text): {b.query_wolfram('Poland')}")
+    print(f"Poland (should fetch image): {b.query_wolfram('Poland', True)}")
+
+temp_test()
