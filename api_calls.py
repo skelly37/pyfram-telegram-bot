673f370f9a47e75093c6a364c87eb20b268403cb
Kamil <skelly37@protonmail.com>
2022-02-05 13:00:44 +0000

    Update api_calls.py

--- api_calls.py
+++ api_calls.py
@@ -1,7 +1,26 @@
 from requests import get
+from urllib.request import urlretrieve
 
-appID = #put API key here
+appID = #your appID
 
-def get_short_anwser(query):
+def short_anwser(query):
 	query = "https://api.wolframalpha.com/v1/result?appid=" + appID + "&i=" + query
 	return get(query).text
+
+def get_image(query):
+	filename = query + ".png"
+	
+	query = "https://api.wolframalpha.com/v1/simple?appid=" + appID + "&i=" + query
+	query += "&background=F5F5F5"
+	query += "&fontsize=20"
+	
+	urlretrieve(query, filename)
+	return filename
+
+def query_wolfram(query, is_image=False):
+	if not is_image:
+		out = short_anwser(query)
+		if out.strip() != "No short answer available":
+			return out
+
+	return "Query result saved in: " + get_image(query)
