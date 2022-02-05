df59b72f6d4254a39aaddea3610120500ed83df4
mk00pl <83863599+mk00pl@users.noreply.github.com>
2022-02-05 14:41:33 +0100

    added class and fixed a typo

--- api_calls.py
+++ api_calls.py
@@ -2,23 +2,27 @@ from requests import get
 from urllib.request import urlretrieve
 from urllib.parse import quote_plus
 
-appID = # your appID
+class Bot:
 
-def short_anwser(query):
-	query = "https://api.wolframalpha.com/v1/result?appid=" + appID + "&i=" + query
-	return get(query).text
+    def __init__(self,appid):
+        self.appid = appid        
 
-def get_image(query,filename):
-        filename = "{}.png".format(filename)
-        api_url = "https://api.wolframalpha.com/v1/simple?appid={}&i={}&background=F5F5F5&fontsize=20"
-        query = api_url.format(appID,quote_plus(query))
-        urlretrieve(query, filename)
-        return filename
+    def short_answer(self,sequery):
+            url = "https://api.wolframalpha.com/v1/result?appid={}&i={}"
+            query = url.format(self.appid,quote_plus(sequery))
+            return get(query).text
 
-def query_wolfram(query, is_image=False):
-	if not is_image:
-		out = short_anwser(query)
-		if out.strip() != "No short answer available":
-			return out
-                    
-	return "Query result saved in: " + get_image(query)
+    def get_image(self,query,filename):
+            filename = "{}.png".format(filename)
+            api_url = "https://api.wolframalpha.com/v1/simple?appid={}&i={}&background=F5F5F5&fontsize=20"
+            query = api_url.format(self.appID,quote_plus(query))
+            urlretrieve(query, filename)
+            return filename
+
+    def query_wolfram(self,query, is_image=False):
+            if not is_image:
+                    out = short_anwser(query)
+                    if out.strip() != "No short answer available":
+                            return out
+                        
+            return "Query result saved in: " + get_image(query)
