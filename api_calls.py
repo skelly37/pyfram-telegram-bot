cebaa5aee4c31e6fdd65506a685cbad2ff56333b
skelly37 <skelly37@protonmail.com>
2022-02-08 18:04:26 +0100

    replaced requests with built-in urllib, updated .gitignore, created requirements.txt, put api key in gitignored api_key.txt, created better test case

--- api_calls.py
+++ api_calls.py
@@ -1,25 +1,25 @@
-from requests import get
-from urllib.request import urlretrieve
+from urllib.request import urlretrieve, urlopen, Request
 from urllib.parse import quote_plus
-
+import telebot
 
 class Bot:
-    def __init__(self, appID):
+    def __init__(self, appID: str) -> None:
         self.appID = appID
 
-    def __short_anwser(self, sequery):
+    def __short_anwser(self, query: str) -> str:
         url = "https://api.wolframalpha.com/v1/result?appid={}&i={}"
-        query = url.format(self.appID, quote_plus(sequery))
-        return get(query).text
+        query = url.format(self.appID, quote_plus(query))
+        response = urlopen(Request(query))
+        return response.read().decode()
 
-    def __get_image(self, query):
+    def __get_image(self, query: str) -> str:
         filename = "{}.png".format(query)
         api_url = "https://api.wolframalpha.com/v1/simple?appid={}&i={}&background=F5F5F5&fontsize=20"
         query = api_url.format(self.appID, quote_plus(query))
         urlretrieve(query, filename)
         return filename
 
-    def query_wolfram(self, query, is_image=False):
+    def query_wolfram(self, query: str, is_image=False) -> str:
         if not is_image:
             out = self.__short_anwser(query)
             if out.strip() != "No short answer available":
@@ -27,10 +27,11 @@ class Bot:
 
         return "Query result saved in: " + self.__get_image(query)
 
-def temp_test():
-    b = Bot("APP_ID")
+def temp_test() -> None:
+    appid = open("api_key.txt").readline().strip()
+    b = Bot(appid)
     print(f"2+2= {b.query_wolfram('2+2')}")
-    print(f"Poland (should return text): {b.query_wolfram('Poland')}")
-    print(f"Poland (should fetch image): {b.query_wolfram('Poland', True)}")
+    print(f"Poland (should return text): {b.query_wolfram('binomial coefficient')}")
+    print(f"Poland (should fetch image): {b.query_wolfram('binomial coefficient', True)}")
 
 temp_test()
