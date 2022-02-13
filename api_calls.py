310ef38edf055386f9eb5f7ee5db4c7b2913926c
skelly37 <skelly37@protonmail.com>
2022-02-13 17:36:04 +0100

    type-checking

--- api_calls.py
+++ api_calls.py
@@ -3,30 +3,32 @@ from requests import get
 from random import randint
 from time import sleep
 
+from typing import List, Sequence
+
 
 class WolframBot:
-    ERROR_MSG = "Error processing your request"
-    NO_STEPS_MSG = "No step by step solution available"
-    NO_SHORT_MSG = "No short answer available"
-    RESULT_SAVED_MSG = "Query result saved in: "
+    ERROR_MSG: str = "Error processing your request"
+    NO_STEPS_MSG: str = "No step by step solution available"
+    NO_SHORT_MSG: str = "No short answer available"
+    RESULT_SAVED_MSG: str = "Query result saved in: "
 
-    def __init__(self, app_ids) -> None:
-        self.__app_ids = app_ids
-        self.__num_of_appids = len(self.__app_ids)
+    def __init__(self, app_ids: Sequence[str]) -> None:
+        self.__app_ids: Sequence[str] = app_ids
+        self.__num_of_appids: int = len(self.__app_ids)
 
     def __get_random_id(self) -> str:
         return self.__app_ids[randint(0, self.__num_of_appids - 1)]
 
     def __short_answer(self, query: str) -> str:
-        url = "https://api.wolframalpha.com/v1/result?appid={}&i={}"
-        id = self.__get_random_id()
+        url: str = "https://api.wolframalpha.com/v1/result?appid={}&i={}"
+        id: str = self.__get_random_id()
         query = url.format(id, quote_plus(query))
         return get(query).text
 
     def __get_image(self, query: str) -> str:
-        filename = "{}.png".format(query)
-        api_url = "https://api.wolframalpha.com/v1/simple?appid={}&i={}&background=F5F5F5&fontsize=20"
-        id = self.__get_random_id()
+        filename: str = "{}.png".format(query)
+        api_url: str = "https://api.wolframalpha.com/v1/simple?appid={}&i={}&background=F5F5F5&fontsize=20"
+        id : str= self.__get_random_id()
         query = api_url.format(id, quote_plus(query))
         r = get(query, stream=True)
         if r.status_code == 200:
@@ -36,9 +38,9 @@ class WolframBot:
             return filename
         return self.ERROR_MSG
 
-    def __get_url_of_steps_image(self, img_xml):
-        img_list = img_xml.split("\n")
-        url_index = -1
+    def __get_url_of_steps_image(self, img_xml: str) -> str:
+        img_list: List[str] = img_xml.split("\n")
+        url_index: int = -1
         for i in range(len(img_list)):
             if "Possible intermediate steps" in img_list[i]:
                 url_index = i + 1
@@ -47,20 +49,20 @@ class WolframBot:
         if url_index == -1:
             return self.NO_STEPS_MSG
 
-        url_raw = str(img_list[url_index]).strip()
-        url = url_raw.replace(' ', '').replace('<imgsrc=', '').replace('<img src=', '').replace("'", "").replace('"',
+        url_raw: str = str(img_list[url_index]).strip()
+        url: str = url_raw.replace(' ', '').replace('<imgsrc=', '').replace('<img src=', '').replace("'", "").replace('"',
                                                                                                                  '')
 
         return url
 
     def get_step_by_step(self, query: str) -> str:
-        filename = "{}.png".format(query)
-        api_url = "https://api.wolframalpha.com/v2/query?appid={}&input={}&podstate=Step-by-step+solution&format=image&totaltimeout=2&background=F5F5F5&fontsize=20"
-        id = self.__get_random_id()
+        filename: str = "{}.png".format(query)
+        api_url: str = "https://api.wolframalpha.com/v2/query?appid={}&input={}&podstate=Step-by-step+solution&format=image&totaltimeout=2&background=F5F5F5&fontsize=20"
+        id: str = self.__get_random_id()
 
         query = api_url.format(id, quote_plus(query))
         response = get(query)
-        img_src = self.__get_url_of_steps_image(response.text)
+        img_src: str = self.__get_url_of_steps_image(response.text)
         sleep(4)
 
         if img_src != self.NO_STEPS_MSG:
@@ -78,13 +80,13 @@ class WolframBot:
             return img_src
         return self.ERROR_MSG
 
-    def query_wolfram(self, query: str, is_image=False, inline_mode=False) -> str:
+    def query_wolfram(self, query: str, is_image: bool = False, inline_mode: bool = False) -> str:
         if not is_image:
-            out = self.__short_answer(query)
+            out: str = self.__short_answer(query)
             if out.strip() != self.NO_SHORT_MSG or inline_mode:
                 return out
 
-        image_response = self.__get_image(query)
+        image_response: str = self.__get_image(query)
         if image_response != self.ERROR_MSG:
             return self.RESULT_SAVED_MSG + image_response
 
--- pyfram_bot.py
+++ pyfram_bot.py
@@ -4,82 +4,86 @@ thus it requires no domain. Feel free to run it on, e.g., your own PC
 """
 
 
-import telebot
+import telebot  # type: ignore
 from os import system
 from api_calls import WolframBot
+from typing import List, Dict
 
 if __name__ == "__main__":
-    TOKEN = open("./bot_token.txt").readline().strip()
-    WHITELIST = [x.strip() for x in open("./whitelist.txt").readlines()]
-    API_KEYS = [x.strip() for x in open("./api_key.txt").readlines()]
-    wolfram = WolframBot(API_KEYS)
+    TOKEN: str = open("./bot_token.txt").readline().strip()
+    WHITELIST: List[str] = [x.strip() for x in open("./whitelist.txt").readlines()]
+    API_KEYS: List[str] = [x.strip() for x in open("./api_key.txt").readlines()]
+    wolfram: WolframBot = WolframBot(API_KEYS)
 
-bot = telebot.TeleBot(TOKEN, parse_mode="MARKDOWN")
+# noinspection PyUnboundLocalVariable
+bot: telebot.TeleBot = telebot.TeleBot(TOKEN, parse_mode="MARKDOWN")
 
-NOT_AUTHORIZED_MSG = "Sorry but you're not authorized to use this bot :)"
-WELCOME_MSG = """Welcome to WolframAlpha bot. Only text input is supported.
+NOT_AUTHORIZED_MSG: str = "Sorry but you're not authorized to use this bot :)"
+WELCOME_MSG: str = """Welcome to WolframAlpha bot. Only text input is supported.
 Default option is to send short message if possible.
 Use /i or /image to force image output
 Use /s or /steps to get step-by-step solution."""
-PROMPT_MSG = "I'm processing your request..."
+PROMPT_MSG: str = "I'm processing your request..."
 
 #message is telebot.types.Message' type
 
-def is_authorized(message) -> bool:
+def is_authorized(message: telebot.types.Message) -> bool:
     return message.from_user.username in WHITELIST
 
 @bot.message_handler(commands=["start", "help"])
-def welcome_and_help(message) -> None:
+def welcome_and_help(message: telebot.types.Message) -> None:
     if is_authorized(message):
         bot.reply_to(message, WELCOME_MSG)
     else:
         bot.reply_to(message, NOT_AUTHORIZED_MSG)
 
 
-def send_prompt_message(message, is_step_by_step=False):
+def send_prompt_message(message: telebot.types.Message, is_step_by_step: bool = False) -> Dict[str, str]:
     data = None
     if is_step_by_step:
         data = bot.reply_to(message, PROMPT_MSG + "\nPlease be patient...")
     else:
         data = bot.reply_to(message, PROMPT_MSG)
+    # noinspection PyTypeChecker
     return dict(message_id = data.message_id, chat_id = data.chat.id)
 
-def delete_prompt_message(prompt):
+def delete_prompt_message(prompt: Dict[str, str]) -> None:
+    # noinspection PyTypeChecker  
     bot.delete_message(prompt["chat_id"], prompt["message_id"])
 
-def remove_commands_from_message(message_text):
+def remove_commands_from_message(message_text: str) -> str:
     return message_text.replace("/image ", "").replace("/steps", "").replace("/i ", "").replace("/s ", "").strip()
 
 
-def get_wolfram_response(message, is_image=False):
-    prompt = send_prompt_message(message)
+def get_wolfram_response(message: telebot.types.Message, is_image: bool = False) -> str:
+    prompt: Dict[str, str] = send_prompt_message(message)
 
-    text = remove_commands_from_message(message.text)
-    search = wolfram.query_wolfram(text, is_image)
-
-    delete_prompt_message(prompt)
-    return search
-
-def get_wolfram_steps(message):
-    prompt = send_prompt_message(message, True)
-
-    text = remove_commands_from_message(message.text)
-    search = wolfram.get_step_by_step(text)
+    text: str = remove_commands_from_message(message.text)
+    search: str = wolfram.query_wolfram(text, is_image)
 
     delete_prompt_message(prompt)
     return search
 
+def get_wolfram_steps(message: telebot.types.Message) -> str:
+    prompt: Dict[str, str] = send_prompt_message(message, True)
 
-def send_image_result(message, search_result):
+    text: str = remove_commands_from_message(message.text)
+    search: str = wolfram.get_step_by_step(text)
+
+    delete_prompt_message(prompt)
+    return search
+
+
+def send_image_result(message: telebot.types.Message, search_result: str) -> None:
     search_result = search_result.replace(WolframBot.RESULT_SAVED_MSG, "")
     bot.send_document(message.chat.id, open(search_result, "rb"), reply_to_message_id=message.id)
     system("rm {}".format('"./' + search_result + '"'))
 
-    
+
 @bot.message_handler(commands=["i", "image"])
-def send_image(message) -> None:
+def send_image(message: telebot.types.Message) -> None:
     if is_authorized(message):
-        search_result = get_wolfram_response(message, True)
+        search_result: str = get_wolfram_response(message, True)
         if search_result != WolframBot.ERROR_MSG:
             send_image_result(message, search_result)
         else:
@@ -88,9 +92,9 @@ def send_image(message) -> None:
         bot.reply_to(message, NOT_AUTHORIZED_MSG)
 
 @bot.message_handler(commands=["s", "steps"])
-def send_steps(message) -> None:
+def send_steps(message: telebot.types.Message) -> None:
     if is_authorized(message):
-        search_result = get_wolfram_steps(message)
+        search_result: str = get_wolfram_steps(message)
         if search_result != WolframBot.ERROR_MSG and search_result != WolframBot.NO_STEPS_MSG:
             send_image_result(message, search_result)
         else:
@@ -99,9 +103,9 @@ def send_steps(message) -> None:
         bot.reply_to(message, NOT_AUTHORIZED_MSG)
 
 @bot.message_handler(func=lambda message: True)
-def handle_query(message) -> None:
+def handle_query(message: telebot.types.Message) -> None:
     if is_authorized(message):
-        search_result = get_wolfram_response(message)
+        search_result: str = get_wolfram_response(message)
 
         if not search_result.startswith(WolframBot.RESULT_SAVED_MSG):
             bot.reply_to(message, search_result)
@@ -111,14 +115,15 @@ def handle_query(message) -> None:
         bot.reply_to(message, NOT_AUTHORIZED_MSG)
 
 @bot.inline_handler(lambda message: len(message.query) > 0)
-def handle_inline_query(inline_query):
-    q = inline_query.query
-    res = wolfram.query_wolfram(query=q, is_image=False, inline_mode=True)
+def handle_inline_query(inline_query: telebot.types.InlineQuery) -> None:
+    q: str = inline_query.query
+    res: str = wolfram.query_wolfram(query=q, is_image=False, inline_mode=True)
     if res != WolframBot.NO_SHORT_MSG:
-        r = telebot.types.InlineQueryResultArticle('1', q, telebot.types.InputTextMessageContent(res))
+        r = telebot.types.InlineQueryResultArticle("1", q, telebot.types.InputTextMessageContent(res))
     else:
-        r = telebot.types.InlineQueryResultArticle('1', res, telebot.types.InputTextMessageContent(res))
+        r = telebot.types.InlineQueryResultArticle("1", res, telebot.types.InputTextMessageContent(res))
 
+    # noinspection PyTypeChecker
     bot.answer_inline_query(inline_query.id, [r])
 
 
